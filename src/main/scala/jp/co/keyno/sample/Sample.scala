0d87c3c5feec51e3033c2fdf81edcdadafc8a2cb
Keyno <kofujiw@gmail.com>
2022-05-05 22:36:12 +0900

    add annotate

--- src/main/scala/jp/co/keyno/sample/Sample.scala
+++ src/main/scala/jp/co/keyno/sample/Sample.scala
@@ -2,6 +2,6 @@ package jp.co.keyno.sample
 
 class Sample(val sampleService: SampleService) {
 
-  def getMessage(value: Option[String]) =
+  def getMessage(value: Option[String]): String =
     sampleService.getMessage(value)
 }
